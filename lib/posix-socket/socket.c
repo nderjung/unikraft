1a273760914170d6d10981acec67efe27e71cfdf
Alexander Jung <alexander.jung@neclab.eu>
2020-09-28 11:58:06 +0200

    posix-socket: Provide recvmsg() method implementation
    Signed-off-by: Alexander Jung <alexander.jung@neclab.eu>

--- lib/posix-socket/socket.c
+++ lib/posix-socket/socket.c
@@ -536,12 +536,41 @@ EXIT_ERR:
 	return ret;
 }
 
+UK_TRACEPOINT(trace_posix_socket_recvmsg, "%d %p %d", int, struct msghdr*, int);
+UK_TRACEPOINT(trace_posix_socket_recvmsg_ret, "%d", int);
+UK_TRACEPOINT(trace_posix_socket_recvmsg_err, "%d", int);
+
 ssize_t
 recvmsg(int sock, struct msghdr *msg, int flags)
 {
-	uk_pr_crit("%s: not implemented\n", __func__);
-	errno = ENOTSUP;
-	return -1;
+	int ret = 0;
+	struct posix_socket_file *file = NULL;
+
+	trace_posix_socket_recvmsg(sock, msg, flags);
+
+	file = posix_socket_file_get(sock);
+	if (PTRISERR(file)) {
+		ret = -1;
+		SOCKET_ERR(PTR2ERR(file), "failed to identify socket descriptor");
+		goto EXIT_ERR;
+	}
+
+	/* Receive a structured message from a socket */
+	ret = posix_socket_recvmsg(file, msg, flags);
+
+	/* release refcount */
+	vfscore_put_file(file->vfs_file);
+
+	if (ret < 0) {
+		uk_pr_err("failed to recvmsg on socket\n");
+		goto EXIT_ERR;
+	}
+
+	trace_posix_socket_recvmsg_ret(ret);
+	return ret;
+EXIT_ERR:
+	trace_posix_socket_recvmsg_err(ret);
+	return ret;
 }
 
 ssize_t
