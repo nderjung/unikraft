9c7ea260c4c02e2ac02096a23b916dc98f1cabfc
Alexander Jung <alexander.jung@neclab.eu>
2020-09-28 13:17:15 +0200

    ukunixsock: Register the socket() method against posix-socket
    This commit initializes the UNIX socket interfaces and provides
    the create method against the posix-socket driver.
    
    The creation of a UNIX socket here initializes the ring buffer
    for message passing as well as a wait queue used for inbound
    connections to the socket itself.
    
    Signed-off-by: Alexander Jung <alexander.jung@neclab.eu>

---
+++ lib/ukunixsock/unixsock.c
@@ -0,0 +1,123 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Authors: Alexander Jung <alexander.jung@neclab.eu>
+ *
+ * Copyright (c) 2020, NEC Laboratories Europe GmbH, NEC Corporation.d
+ *                     All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <uk/mbox.h>
+#include <uk/print.h>
+#include <uk/config.h>
+#include <uk/assert.h>
+#include <uk/socket.h>
+#include <uk/unixsock.h>
+#include <uk/semaphore.h>
+#include <uk/essentials.h>
+#include <uk/arch/limits.h>
+#include <vfscore/dentry.h>
+
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+
+static void *
+unixsock_glue_create(struct posix_socket_driver *d, int family, int type,
+          int protocol)
+{
+  void *ret = NULL;
+  struct unixsock *unsock;
+  UK_ASSERT(family == AF_UNIX);
+
+  /* Unix domain sockets only support SOCK_STREAM and SOCK_DGRAM. */
+  if (((type & SOCK_STREAM) != SOCK_STREAM) &&
+      ((type & SOCK_DGRAM) != SOCK_DGRAM) &&
+      ((type & SOCK_SEQPACKET) != SOCK_SEQPACKET)) {
+    ret = NULL;
+    SOCKET_ERR(ESOCKTNOSUPPORT, "socket type unsupported");
+    goto EXIT;
+  }
+
+  unsock = uk_calloc(d->allocator, 1, sizeof(struct unixsock));
+  if (!unsock) {
+    ret = NULL;
+    SOCKET_ERR(ENOMEM, "could not allocate socket");
+    goto EXIT;
+  }
+
+  /* Initialize unixsock. */
+  unsock->local_addr.sun_family = AF_UNIX;
+  unsock->local_addr.sun_path[0] = '\0';
+  
+  /* Initialize connection wait. */
+  uk_semaphore_init(&unsock->connections, 0);
+
+#ifdef DEBUG_BUFRING
+  uk_mutex_init(&unsock->buflock);
+#endif
+
+  /* Initialize a buffer for this socket. */
+  unsock->buffer = unixsock_ring_alloc(CONFIG_LIBUKUNIXSOCK_BUFLEN, d->allocator
+#ifdef DEBUG_BUFRING
+          , &unsock->buflock
+#endif
+  );
+  if (unsock->buffer == NULL) {
+    ret = NULL;
+    SOCKET_ERR(ENOMEM, "could not allocate socket buffer");
+    goto LOCAL_SOCKET_CLEANUP;
+  }
+
+#if CONFIG_LIBUKSCHED
+	uk_waitq_init(&unsock->state_wq);
+#endif
+
+  /* Set the initial state of the socket. */
+  unsock->state = UNIXSOCK_OPEN;
+
+  /* Return a successfully initialized unix domain socket. */
+  ret = (void *)unsock;
+
+EXIT:
+  return ret;
+
+LOCAL_SOCKET_CLEANUP:
+  uk_free(d->allocator, unsock);
+  goto EXIT;
+}
+
+
+static struct posix_socket_ops unixsock_ops = {
+  /* POSIX interfaces */
+  .create      = unixsock_glue_create,
+};
+
+POSIX_SOCKET_FAMILY_REGISTER(AF_UNIX, &unixsock_ops, NULL);
